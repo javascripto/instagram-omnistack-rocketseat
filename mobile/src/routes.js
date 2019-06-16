b9e6092f9ef57f51d4ef6fcc8db5229687d08639
Yuri Alves <yurialves2@gmail.com>
2019-06-16 00:53:08 -0300

    initial app routes and navigation

---
+++ mobile/src/routes.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import { Image } from 'react-native';
+import { createAppContainer, createStackNavigator } from 'react-navigation';
+
+import New from './pages/New';
+import Feed from './pages/Feed';
+
+import logo from './assets/logo.png';
+
+
+// Alguns tipos de navegação:
+// createStackNavigator, createBottomTabNavigator, createSwitchNavigator, createDrawerNavigator
+export default createAppContainer(
+  createStackNavigator({
+    Feed,
+    New,
+  }, {
+    defaultNavigationOptions: {
+      headerBackTitle: null,
+      headerTintColor: '#000', 
+      headerTitle: <Image style={{ marginHorizontal: 20 }} source={logo} />,
+    },
+    mode: 'modal'
+  })
+);
