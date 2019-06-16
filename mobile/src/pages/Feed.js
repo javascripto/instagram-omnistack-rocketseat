02cffc9d01b99ff1f673e2f27c9e23af37727d41
Yuri Alves <yurialves2@gmail.com>
2019-06-16 02:47:03 -0300

    feed completed

--- mobile/src/pages/Feed.js
+++ mobile/src/pages/Feed.js
@@ -1,9 +1,14 @@
+import io from 'socket.io-client';
 import React, { Component } from 'react';
 import { View, Text, Image, StyleSheet, TouchableOpacity, FlatList } from 'react-native';
 
-import api from '../services/api';
+import api, { currentOrigin } from '../services/api';
 
+import more from '../assets/more.png';
+import like from '../assets/like.png';
+import send from '../assets/send.png';
 import camera from '../assets/camera.png';
+import comment from '../assets/comment.png';
 
 export default class Feed extends Component {
   static navigationOptions = ({ navigation }) => ({
@@ -19,9 +24,31 @@ export default class Feed extends Component {
   }
 
   async componentDidMount() {
-    const response = await api.get('posts');
+    this.registerToSocket();
 
+    const response = await api.get('posts');
+    
     this.setState({ feed: response.data });
+  }
+
+  registerToSocket = () => {
+    const socket = io(currentOrigin);
+
+    socket.on('post', newPost => {
+      this.setState({ feed: [newPost, ...this.state.feed] });
+    });
+
+    socket.on('like', likedPost => {
+      this.setState({
+        feed: this.state.feed.map(post =>
+          post._id === likedPost._id ? likedPost : post
+        )
+      })
+    });
+  }
+
+  handleLike = (id) => {
+    api.post(`post/${id}/like`)
   }
 
   render() {
@@ -31,7 +58,36 @@ export default class Feed extends Component {
           data={this.state.feed}
           keyExtractor={post => post._id}
           renderItem={({ item }) => (
-            <Text>{item.author}</Text>
+            <View style={styles.feedItem}>
+              <View style={styles.feedItemHeader}>
+                <View style={styles.userInfo}>
+                  <Text style={styles.name}>{item.author}</Text>
+                  <Text style={styles.place}>{item.place}</Text>
+                </View>
+
+                <Image source={more} />
+              </View>
+
+              <Image style={styles.feedImage} source={{ uri: `${currentOrigin}/files/${item.image}` }} />
+
+              <View style={styles.feedItemFooter}>
+                <View style={styles.actions}>
+                  <TouchableOpacity style={styles.action} onPress={() => handleLike(item._id)}>
+                    <Image source={like} />
+                  </TouchableOpacity>
+                  <TouchableOpacity style={styles.action} onPress={() => {}}>
+                    <Image source={comment} />
+                  </TouchableOpacity>
+                  <TouchableOpacity style={styles.action} onPress={() => {}}>
+                    <Image source={send} />
+                  </TouchableOpacity>
+                </View>
+
+                <Text style={styles.likes}>{item.likes} curtidas</Text>
+                <Text style={styles.description}>{item.description}</Text>
+                <Text style={styles.hashtags}>{item.hashtags}</Text>
+              </View>
+            </View>
           )}
         />
       </View>
@@ -41,5 +97,53 @@ export default class Feed extends Component {
 
 
 const styles = StyleSheet.create({
-  
+  container: {
+    flex: 1
+  },
+  feedItem: {
+    marginTop: 20
+  },
+  feedItemHeader: {
+    paddingHorizontal: 15,
+
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+  },
+  name: {
+    fontSize: 14,
+    color: '#000'
+  },
+  place: {
+    fontSize: 12,
+    color: '#666',
+    marginTop: 2
+  },
+  userInfo: {},
+  feedImage: {
+    width: '100%',
+    height: 400,
+    marginVertical: 15
+  },
+  feedItemFooter: {
+    paddingHorizontal: 15
+  },
+  actions: {
+    flexDirection: 'row'
+  },
+  action: {
+    marginRight: 8
+  },
+  likes: {
+    marginTop: 15,
+    fontWeight: 'bold',
+    color: '#000'
+  },
+  description: {
+    lineHeight: 16,
+    color: '#000'
+  },
+  hashtags: {
+    color: '#7159c1'
+  },
 });
--- mobile/src/services/api.js
+++ mobile/src/services/api.js
@@ -1,9 +1,9 @@
 import axios from 'axios';
 
-// const currentOrigin = 'http://localhost:3333'; // rodando emulador
-// const currentOrigin = 'http://10.0.3.2:3333'; // rodando emulador geanymotion
-// const currentOrigin = 'http://10.0.2.2:3333'; // rodando emulador do Android Studio
-const currentOrigin = 'http://192.168.15.14:3333'; // Meu IP local atual
+// export const currentOrigin = 'http://localhost:3333'; // rodando emulador
+// export const currentOrigin = 'http://10.0.3.2:3333'; // rodando emulador geanymotion
+// export const currentOrigin = 'http://10.0.2.2:3333'; // rodando emulador do Android Studio
+export const currentOrigin = 'http://192.168.15.14:3333'; // Meu IP local atual
 
 export default axios.create({
     baseURL: currentOrigin
